1851386fa20ac6314f11b390ecd612242616387a
hongphi <hongphi.math@gmail.com>
2011-07-22 22:04:13 +0700

    optimize import

--- qna/urls.py
+++ qna/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, url
+
 
 urlpatterns = patterns('qna.views',
-    url(r'^$', 'home', name='home-page'),
+    url(r'^$', 'home', name = 'home-page'),
 
 )

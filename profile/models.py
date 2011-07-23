5b236ac8e850d02e59879952ee0eac39ab80397e
hongphi <hongphi.math@gmail.com>
2011-07-24 00:30:53 +0700

    create Profile class

--- profile/models.py
+++ profile/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+
+    user_id = models.ForeignKey(User, unique = True)
+    website = models.CharField(max_length = 200)
+
+
--- profile/views.py
+++ profile/views.py
@@ -1,1 +1,22 @@
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+
+
+@login_required
+def profile_home(request):
+    '''
+    View profile of User.
+    @param request:
+    '''
+    return HttpResponse("")
+
+
+@login_required
+def profile_update(request):
+    '''
+    Update profile of User.
+    @param request:
+    '''
+    return HttpResponse("")

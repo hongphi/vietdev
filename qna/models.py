6d468402040ef99497c92abd92f1f9462b2783d1
phongnt <genycopedison@gmail.com>
2011-07-23 23:34:58 +0700

    - add model Question

--- qna/models.py
+++ qna/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+    author = models.ForeignKey(Member)
+    day = models.DateField()
+    num_of_answers = models.IntegerField()
+
+    def __unicode__(self):
+        return self.title

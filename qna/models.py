f24555848167c501bbf6e45ed3e5b4ce5c7a15ca
phongnt <genycopedison@gmail.com>
2011-07-25 23:23:08 +0700

    Modify models: Question and Answer, add related_name

--- qna/models.py
+++ qna/models.py
@@ -3,13 +3,13 @@ from django.contrib.admin.models import User
 
 # Create your models here.
 class Question(models.Model):
-    title       = models.CharField(max_length = 100)    # Title of question
-    date        = models.DateField(auto_now = True)     # Day ask
-    content     = models.TextField(max_length = 1000)   # Content of question
-    author      = models.ForeignKey(User)               # Who is ask this question
-    likes       = models.ManyToManyField(User)          # User like this question
-    tags        = models.CharField(max_length = 100)    # This question can be in tags
-    bonus       = models.IntegerField()                 # Bonus points of this question
+    title       = models.CharField(max_length = 100)                        # Title of question
+    date        = models.DateField(auto_now = True)                         # Day ask
+    content     = models.TextField(max_length = 1000)                       # Content of question
+    author      = models.ForeignKey(User, related_name = "q_author")        # Who is ask this question
+    likes       = models.ManyToManyField(User, related_name = "q_likes")    # User like this question
+    tags        = models.CharField(max_length = 100)                        # This question can be in tags
+    bonus       = models.IntegerField()                                     # Bonus points of this question
 
     def __unicode__(self):
         return self.title
@@ -18,11 +18,11 @@ class Question(models.Model):
         return Answer.objects.filter(question = self)
     
 class Answer(models.Model):
-    date        = models.DateField(auto_now = True)     # Answer day
-    content     = models.TextField(max_length = 1000)   # Content of answer
-    author      = models.ForeignKey(User)               # Who is author of this answer
-    likes       = models.ManyToManyField(User)          # Users like this answer
-    question    = models.ForeignKey(Question)           # Question of this answer
+    date        = models.DateField(auto_now = True)                         # Answer day
+    content     = models.TextField(max_length = 1000)                       # Content of answer
+    author      = models.ForeignKey(User, related_name = "a_author")        # Who is author of this answer
+    likes       = models.ManyToManyField(User, related_name = "a_likes")    # Users like this answer
+    question    = models.ForeignKey(Question)                               # Question of this answer
 
     def __unicode__(self):
         return self.author

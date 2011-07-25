2a81afc52f8418f2eb271d6f66ffc9eba14fd491
hongphi <hongphi.math@gmail.com>
2011-07-25 23:53:21 +0700

    update urlpattern

--- profile/urls.py
+++ profile/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 
-urlpatterns = patterns('profile.view',
+urlpatterns = patterns('profile.views',
 
     url(r'^profile/$', 'profile_home'),
     url(r'^profile/update/$', 'profile_update'),

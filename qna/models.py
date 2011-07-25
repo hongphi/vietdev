e726999b491d796b73a3a170e0ba2418993374d9
hongphi <hongphi.math@gmail.com>
2011-07-25 23:26:13 +0700

    Definition Profile model

--- qna/models.py
+++ qna/models.py
@@ -4,11 +4,11 @@ from django.contrib.admin.models import User
 # Create your models here.
 class Question(models.Model):
     title       = models.CharField(max_length = 100)    # Title of question
-    date        = models.DateField(auto_now = True)     # Day ask
+    date        = models.DateField()                    # Day ask
     content     = models.TextField(max_length = 1000)   # Content of question
-    author      = models.ForeignKey(User)               # Who is ask this question
-    likes       = models.ManyToManyField(User)          # User like this question
-    tags        = models.CharField(max_length = 100)    # This question can be in tags
+    author      = models.ForeignKey(User)               # Who is ask this question\
+    likes       = models.ForeignKey(User)               # User like this question
+    tags        = models.CharField(100)                 # This question can be in tags
     bonus       = models.IntegerField()                 # Bonus points of this question
 
     def __unicode__(self):
@@ -18,7 +18,7 @@ class Question(models.Model):
         return Answer.objects.filter(question = self)
     
 class Answer(models.Model):
-    date        = models.DateField(auto_now = True)     # Answer day
+    date        = models.DateField()                    # Answer day
     content     = models.TextField(max_length = 1000)   # Content of answer
     author      = models.ForeignKey(User)               # Who is author of this answer
     likes       = models.ManyToManyField(User)          # Users like this answer

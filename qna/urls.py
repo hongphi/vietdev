a0deb9ef226aa551dd74d5fa57bb491aecbc32e1
phongnt <genycopedison@gmail.com>
2011-07-26 23:34:18 +0700

    Add page ask a question

--- qna/urls.py
+++ qna/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns('qna.views',
-    url(r'^$', 'home', name = 'home-page'),
-
+    #url(r'^$', 'home', name = 'home-page'),
+    url(r'^$','ask'),
 )
--- qna/views.py
+++ qna/views.py
@@ -8,7 +8,8 @@ from django.template.defaultfilters import slugify
 from django.views.decorators.csrf import csrf_protect
 
 import json
-
+from qna.models import Question
+import datetime
 
 def home(request):
     """
@@ -17,3 +18,11 @@ def home(request):
     return render_to_response('home.html',
         {},
         context_instance = RequestContext(request))
+
+def ask(request):
+    if 'q' in request.GET and 't' in request.GET:
+        q = request.GET['q']
+        t = request.GET['t']
+        if q and t:
+            question = Question(title = t, content=q)
+    return render_to_response('ask.html',{'title' : "",'question' : ""})

5582264bd608930d473b0de0ae616b70fc652e4e
phongnt <genycopedison@gmail.com>
2011-07-27 23:07:27 +0700

    Add page list all questions

--- qna/urls.py
+++ qna/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('qna.views',
     #url(r'^$', 'home', name = 'home-page'),
     url(r'^$','ask'),
+    url(r'list/','list'),
 )

5c94d4bdc7aad4457f7b5a70ba2a5e7829ee4dde
hongphi <hongphi.math@gmail.com>
2011-07-29 00:17:26 +0700

    create sustom template filters

--- qna/templatetags/tagslib.py
+++ qna/templatetags/tagslib.py
@@ -1,8 +1,17 @@
 from django import template
+from qna.models import Question, Answer
 
 register = template.Library()
 
 @register.filter
 def test(value, agr):
     return value + agr
+
+@register.filter
+def count_answer(obj):
+    if isinstance(obj, Question):
+        return Answer.objects.filter(question = obj).count()
+    else:    
+        raise TypeError("Object input must be a instance of Question!")
+ 
 

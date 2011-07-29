93de7d0f98f49f2d0075c54aee52fb50487dbb34
hongphi <hongphi.math@gmail.com>
2011-07-29 21:21:07 +0700

    config media file

--- settings.py
+++ settings.py
@@ -73,6 +73,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 # Additional locations of static files
 STATICFILES_DIRS = (
     os.path.join(PROJECT_DIR, 'static_source'),
+    os.path.join(PROJECT_DIR, 'media'),
 )
 
 # List of finder classes that know how to find static files in
--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.sitemaps import Sitemap
 from forms import RegistrationFormUtfUsername
+from django.conf import settings
 
 
 from django_authopenid.urls import urlpatterns as authopenid_urlpatterns
@@ -12,9 +13,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-
+                       
     url(r'^qna/', include('qna.urls')),
     url(r'^profile/', include('profile.urls')),
     url(r'^admin/', include(admin.site.urls)),
     (r'^account/', include(authopenid_urlpatterns)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
 )

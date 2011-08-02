b112e448d6d66e801a6b98b04e92f9d364d3b496
hongphi <hongphi.math@gmail.com>
2011-08-02 23:00:53 +0700

    customize profile and url

--- profile/urls.py
+++ profile/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('profile.views',
 
     url(r'^$', 'profile_home'),
-    url(r'^update/$', 'profile_update'),
+    url(r'^(?P<username>\w+)/$', 'view_profile'),
+    url(r'^(?P<username>\w+)/update/$', 'profile_update'),
 
 )
--- profile/views.py
+++ profile/views.py
@@ -6,6 +6,7 @@ from profile.models import Profile, Activity
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from profile.forms import ProfileForm
+from django.contrib.auth.models import User
 
 
 
@@ -15,12 +16,19 @@ def profile_home(request):
     View profile of User.
     @param request:
     '''
+    return HttpResponseRedirect('/profile/' + request.user.username)
+
+@login_required
+def view_profile(request, username):
     try:
-        profile, created = Profile.objects.get_or_create(user = request.user)
+        required_user = User.objects.get(username = username)
+        profile, created = Profile.objects.get_or_create(user = required_user)
         if created:
-            active = Activity(user = request.user)
+            active = Activity(user = required_user)
             active.save()
-            return HttpResponseRedirect('/profile/update/')            
+            if required_user == request.user:
+                return HttpResponseRedirect('/profile/update/')
+                        
     except Exception as e:
         raise Http404()
     
@@ -30,7 +38,7 @@ def profile_home(request):
 
 @login_required
 @csrf_protect
-def profile_update(request):
+def profile_update(request, username):
     '''
     Update profile of User.
     @param request:
@@ -44,7 +52,7 @@ def profile_update(request):
         profile_form = ProfileForm(request.POST, request.FILES, instance = profile)
         if profile_form.is_valid():
             profile_form.save()
-            return HttpResponseRedirect('/profile/')
+            return HttpResponseRedirect('/profile/' + username)
     else:        
         profile_form = ProfileForm(instance = profile)
             
